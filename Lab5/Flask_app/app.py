9fc152f603d0573c8aa74ad087bd7393585f20d0
sdiazgo <79860678+sdiazgo@users.noreply.github.com>
2021-04-09 22:51:30 -0500

    Add files via upload

---
+++ Lab5/Flask_app/app.py
@@ -0,0 +1,37 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+
+def index():
+    message = ''
+    if flask.request.method == 'POST':
+        limite_inferior = flask.request.form['limite_inferior']
+        limite_superior = flask.request.form['limite_superior']
+        coeficiente = flask.request.form['coeficiente']
+        n_subintervalos = flask.request.form['n_subintervalos']
+        result = calc_rt(float(limite_inferior), float(limite_superior), float(coeficiente), float(n_subintervalos))
+        message = 'La integral es: ' +  str(result)
+    return flask.render_template('index.html', message=message)
+
+if __name__ == '__main__':
+    app.run()
+
+
+def calc_rt(li, ls, co, n):
+    lim_i=li
+    lim_s=ls
+    coef=co
+    subin=n
+    dx=(lim_s-lim_i)/subin
+    if lim_i>lim_s:
+        lim_i=ls
+        lim_s=li
+        dx=(lim_s-lim_i)/subin
+    resultado = 0
+    i = lim_i
+    while i < lim_s:
+        resultado = resultado + dx*((((i+dx)**2)+(i**2))/2)
+        i +=dx
+    return coef*resultado
